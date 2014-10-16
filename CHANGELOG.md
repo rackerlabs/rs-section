bf2252f1415de5f5f15c2775aea898851d88b8ad
Brad Gignac <bgignac@bradgignac.com>
2014-10-16 16:39:55 -0400

    chore(release): release v0.1.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # CHANGELOG
 
+## v0.1.1 (October 16, 2014)
+
+- Remove focus outline from non-collapsible sections.
+
 ## v0.1.0 (October 15, 2014)
 
 - Initial release of rs-section directive.
--- dist/rs-section.js
+++ dist/rs-section.js
@@ -3,7 +3,9 @@ angular.module('rs.section', []).run(function () {
 
   var styleContent, styleTag;
 
-  styleContent = document.createTextNode('rs-section { display: block; } rs-section + rs-section { border-top: 1px solid #eee; }');
+  styleContent = document.createTextNode('rs-section { display: block; } \
+    rs-section + rs-section { border-top: 1px solid #eee; } \
+    .rs-detail-section-header { outline: none; }');
   styleTag = document.createElement('style');
   styleTag.type = 'text/css';
   styleTag.appendChild(styleContent);
