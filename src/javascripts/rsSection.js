6fd2922ca7387ab5c00945c1334bb6abf951dc5f
Eddy Hernandez <eddy.hernandez@rackspace.com>
2015-02-25 11:52:11 -0800

    fix(SectionController): watch collapsible & disable toggle when loading

--- src/javascripts/rsSection.js
+++ src/javascripts/rsSection.js
@@ -4,7 +4,8 @@ angular.module('rs.section').directive('rsSection', function () {
   return {
     scope: {
       title: '@',
-      description: '@'
+      description: '@',
+      collapsible: '@'
     },
     restrict: 'EA',
     controller: 'SectionController',
